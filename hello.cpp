5167b3da97aaaad31fd7a7ff009840980cbe80e8
Jlowzow <joachim@lowzow.org>
2016-08-24 11:02:12 +0200

    nå med pi

--- hello.cpp
+++ hello.cpp
@@ -17,12 +17,17 @@ int main() //hovedprogram
     double areal = l * b;
     double div = l / b;
     double sum = l + b;
+    double sirkel = pi*b*b;
 
     l++; // øker med 1
     b--; // minker med 1
     
 
     cout << "Arealet er:  " << areal << endl;
+    cout << "Arealet til sirkel en er:  " << sirkel << endl;
+    cout << "brøk " << div << endl;
+    cout << "sum " << sum << endl;
+    cout << "pi " << pi << endl;
     return 0; 
 
 }
