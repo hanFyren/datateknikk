78921be81f558dcf82950db56ab2042a77e549b9
Jlowzow <joachim@lowzow.org>
2016-09-07 13:59:52 +0200

    modified:   veketrelowz.cpp

--- veketrelowz.cpp
+++ veketrelowz.cpp
@@ -1,14 +1,16 @@
 // veketrelowz.cpp
 
+
 #include <iostream>
 #include <string>
+#include <cmath>
 using namespace std;
 
-string signtest(int n){
-    string svar = "positivt";
+string signtest(int n){      // funksjon som tester fortegn på integers, returnerer string
+    string svar = "positivt"; // definerer stringen svar, og setter den til positivt.
 
     if(n < 0){
-       svar = "negativt";
+       svar = "negativt";   // Endrer svar til negativt om dette er tilfelle
     }
 
     return svar; 
@@ -18,18 +20,27 @@ string signtest(int n){
 
 int main() {
     
+    cout << "Dette programmet tester fortegn på heltall og lager løpende gjennomsnitt" << endl;
+    cout << "avslutt med 999" << endl;
+    // Definerer variabler 
     int n = 0;
     int sum = 0;
-    int x;
+    double x;
     double gjennomsnitt;
 
-        while(cin >> x && x != 999) //leser inn tall så lenge det er integers og ikke 999
-    {
-        n++;
-        sum = sum + x;
-        gjennomsnitt = sum / (double)n;
-        cout << signtest(x)+ " " << gjennomsnitt << endl;
-    }
+        while(cin >> x && x != 999 && floor(x) == x) //leser inn tall så lenge det er integers og ikke 999
+        {
+        n++;                        //Teller syklusser
+        sum = sum + x;              //Summerer alle tallene
+        gjennomsnitt = sum / (double)n;                     //Regner ut gjennomsnitt i farten
+        cout << signtest(x)+ ", gjennomsnitt: " << gjennomsnitt << endl;   //Skriver ut signtest av x og gjennomsnittet
+        }   
+    
+    if(n == 0){
+        cout << "du leste ikke inn noe heltall" << endl;
+    } else{
+        cout << "avsluttet med kode 999 eller feil input" << endl;
 
     return 0;
-}
+    }
+}   
